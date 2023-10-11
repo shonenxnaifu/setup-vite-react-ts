171cc990a8153abbd5f59f59bc6f41d8fd62c4e6
Pawitra Warda <pawitrawarda@gmail.com>
2023-10-11 16:48:24 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <form>
+      <input
+        id="dinosaur_name"
+        name="dinosaur_name"
+        className="dinosaur-input"
+        aria-describedby="dinosaur_name_info"
+      />
+    </form>
+  );
+}
+
+export default App;
